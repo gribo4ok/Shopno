af0202cb85371ba786443b647d5159439cfaf179
gribo4ok <64685464+gribo4ok@users.noreply.github.com>
2020-05-22 14:20:40 +0300

    Merge pull request #3 from gribo4ok/homepage.html
    Update homepage.html

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ $(function() {
 		$('.menu__list, .header__nav').toggleClass('activ');
 	})
 
-
+	$('.slick-slider').slick({
+		dots:true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+	});
 	
 });
